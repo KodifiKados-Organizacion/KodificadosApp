6b3f6578d3638828e6df86feb4c7a20201acb75f
William Rodriguez <williamrodriguez997@gmail.com>
2022-10-06 20:00:08 -0500

    primer comit del grupo.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('Hola Mundo');
+
+
+// Language: javascript
+// Path: Ciclo4_Misiontic2022\index.js
+console.log('Hola Mundo');
+
+const saludar = (nombre) => {
+    return `Hola ${nombre}`;
+}
+
+var nombre = prompt('Ingrese su nombre');
+console.log(saludar(nombre));
