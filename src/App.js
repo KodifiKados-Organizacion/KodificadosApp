f94c8369ab87259775318a505ade59340d16f91c
William Rodriguez <williamrodriguez997@gmail.com>
2022-10-13 16:03:02 -0500

    pagina inicio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
+import work from './assets/imgs/Video Conference Presentation.gif'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Bienvenidos a la app de Kodificados </h1>
+
+          <p> ... en desarrollo</p>
+          <img src={ work } alt="trabajando" />
+
       </header>
     </div>
   );
