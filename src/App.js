35873a9efb6655c5cf94d8834da28598ca821173
Camila Cavanzo <quinteroc070@gmail.com>
2022-10-13 21:02:28 -0500

    se agrega fecha en la pag principal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import work from './assets/imgs/Video Conference Presentation.gif'
 import './App.css';
 
 function App() {
+  let date = Date();
   return (
     <div className="App">
       <header className="App-header">
           <h1>Bienvenidos a la app de Kodifikados </h1>
 
-          <p> ... en desarrollo</p>
+          <p> ... en desarrollo <br></br> { date }</p>
           <img src={ work } alt="trabajando" />
 
       </header>
