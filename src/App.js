5575c152c2ec4163079e12e7e2a991dcb0d53472
William Rodriguez <williamrodriguez997@gmail.com>
2022-10-19 21:02:46 -0500

    Varios Cambios e inicio de diseño del footer y se agrega el logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+import logo from './assets/imgs/logo10_19_20826.png';
 import work from './assets/imgs/Video Conference Presentation.gif'
 import './styles/App.css';
 import {Header} from './components/Header/Header';
 import { Footer } from './components/Footer/Footer'
 function App() {
+  let date = Date();
   return (
     <div className="App">
         <Header />
+        <div className="App-header">
+          <span className='container-logo'>
+          <img src={ logo } alt="logo" className='logo' />
+          <h1>Bienvenidos a la app de Kodifikados </h1>
+          </span>
+          <p> ... en desarrollo <br></br> { date }</p>
+          <img src={ work } alt="trabajando" />
+
+        </div>
         <Footer />
     </div>
   );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,43 @@
 import React from 'react'
 import '../../styles/Footer.css'
 
+
 export const Footer = () => {
   return (
     <div className='footer'>
-        <h3>footer</h3>
+       <div className='container-redes'>
+          <span>
+            <a href="#">
+            <ion-icon name="logo-facebook" size='large'>Facebook</ion-icon>
+            </a>
+          </span>
+          <span>
+            <a href="#">
+            <ion-icon name="logo-twitter" size='large'>Twitter</ion-icon>
+            </a>
+          </span>
+          <span>
+            <a href="#">
+            <ion-icon name="logo-instagram" size='large'>Instagram</ion-icon>
+            </a>
+          </span>
+          <span>
+            <a href="#">
+            <ion-icon name="logo-youtube" size='large'>YouTube</ion-icon>
+            </a>
+          </span>
+       </div>
+       <div className='container-contacto'>
+        <span className='containers-contacto'>
+          <h3>Quienes somos?</h3>
+          <p>somos una empresa creada para solucionar los problemas de pequeñas empresas para entrar en la web, ofrecemos soluciones de diseño, creacion y mantenimiento de plataformas web y movil.</p>
+        </span>
+        <span className='containers-contacto'>
+          <h3>Contactanos</h3>
+          <h6>Tel: 3213019261 </h6>
+          <h6>Dir: Cll 123 # 45 - 67</h6>
+        </span>
+       </div>
     </div>
   )
 }
