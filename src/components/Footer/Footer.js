fb8cd3aced0038f06b1773043d2efbd9d4682c35
William Rodriguez <williamrodriguez997@gmail.com>
2022-10-20 10:16:46 -0500

    se agrega pages para clientes y administradores.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import '../../styles/Footer.css'
+import logo  from '../../assets/imgs/logo10_19_20826.png'
 
 
 export const Footer = () => {
   return (
     <div className='footer'>
+       <div className='container-contacto'>
+        <span>
+          <img src={logo} alt="logo KodifiKados" className='logo-footer' />
+        </span>
+        <span className='containers-contacto'>
+          <h3>Quienes somos?</h3>
+          <p>somos una empresa creada para solucionar los problemas de pequeñas empresas para entrar en la web, ofrecemos soluciones de diseño, creacion y mantenimiento de plataformas web y movil.</p>
+        </span>
+        <span className='containers-contacto'>
+          <h3>Contactanos</h3>
+          <h6>Tel: 3213019261 </h6>
+          <h6>Dir: Cll 123 # 45 - 67</h6>
+        </span>
+       </div>
        <div className='container-redes'>
           <span>
             <a href="#">
@@ -26,17 +41,6 @@ export const Footer = () => {
             <ion-icon name="logo-youtube" size='large'>YouTube</ion-icon>
             </a>
           </span>
-       </div>
-       <div className='container-contacto'>
-        <span className='containers-contacto'>
-          <h3>Quienes somos?</h3>
-          <p>somos una empresa creada para solucionar los problemas de pequeñas empresas para entrar en la web, ofrecemos soluciones de diseño, creacion y mantenimiento de plataformas web y movil.</p>
-        </span>
-        <span className='containers-contacto'>
-          <h3>Contactanos</h3>
-          <h6>Tel: 3213019261 </h6>
-          <h6>Dir: Cll 123 # 45 - 67</h6>
-        </span>
        </div>
     </div>
   )
