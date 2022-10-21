4010f9cb581d0cc1b617cadeeac6e89f58f12851
Andres Felipe Mendoza <ibaguetolima@hotmail.com>
2022-10-21 14:57:01 -0500

    Navbar sin conexion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './assets/imgs/logo10_19_20826.png';
 import work from './assets/imgs/Video Conference Presentation.gif'
 import './styles/App.css';
+import React, { Component }  from 'react';
 import {Header} from './components/Header/Header';
 import { Footer } from './components/Footer/Footer'
 function App() {
   let date = Date();
+
+  
   return (
     <div className="App">
         <Header />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,55 @@
 import React from 'react'
+import About from '../../pages/About/About'
+import admins from '../../pages/Admins/admins'
+import clientes from '../../pages/clientes/clientes'
+import index from '../../index'
 import '../../styles/Header.css'
 
 export const Header = () => {
+  let Component
+  switch (window.location.pathname){
+    
+    case "/":
+      Component = <index/>
+      break
+      case "/clientes":
+        Component = clientes
+        break
+        case "/admins":
+          Component =  admins
+          break
+          case "/About":
+            Component = About
+            break
+  }
   return (
-    <div>
+
+    <nav className="nav">
+      <a href="/" className='site-title'>Kodificados</a>
+      <ul>
+        <li>
+          <a href="#">Clientes</a>
+          </li>
+          <li>
+          <a href="#">Admin</a>
+        </li>
+        <li>
+          <a href="/About">Nosotros</a>
+        </li>
+      </ul>
+
+    </nav>
+
+    
+
+    
+
+    
+/*     <div>
         <header>
             <h1>Bienvenidos</h1>
         </header>
-    </div>
+    </div> */
   )
 }
 
