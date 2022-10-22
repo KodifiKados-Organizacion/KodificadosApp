44a17fc839eb1df5e63e70e479327aa8703a567d
William Rodriguez <williamrodriguez997@gmail.com>
2022-10-22 09:42:22 -0500

    ultimas correcciones

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './styles/App.css';
-import React, { Component }  from 'react';
 import {Header} from './components/Header/Header';
 import { Footer } from './components/Footer/Footer'
 import { Home } from './pages/home/Home';
