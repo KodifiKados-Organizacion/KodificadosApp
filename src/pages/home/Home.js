f58ddee7e2ed692f5fbd9d92422cdd4a3b0016e2
CamiCavaQ <104599956+CamiCavaQ@users.noreply.github.com>
2022-10-22 12:49:25 -0500

    Se hace el carrusel y se cargan imagenes

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,10 @@
 import logo from '../../assets/imgs/logo10_19_20826.png';
 import work from '../../assets/imgs/Video Conference Presentation.gif'
-
+import computador from '../../assets/imgs/Computador1.jpg';
+import telefono from '../../assets/imgs/Telefono.jpg'
+import pcgamer from '../../assets/imgs/pcgamer.jpg'
+import accesorios from '../../assets/imgs/Accesorios.jpg'
+import '../../styles/pages/Home.css'
 export const Home = () => {
     let date = Date();
   return (
@@ -11,6 +15,24 @@ export const Home = () => {
         </span>
         <p> ... en desarrollo <br></br> { date }</p>
         <img src={ work } alt="trabajando" />
-    </div> 
+
+        <div className='titlecarousel'><h2>MAS POPULARES</h2></div>
+        <section className='carousel'>
+        <div className='carousel__container'>
+            <div className='carousel-item'>
+                <img className='carousel-item__img' src={computador} alt='computador' />
+            </div>
+            <div className='carousel-item'>
+              <img className='carousel-item__img' src={telefono} alt='Telefono' />
+            </div>
+            <div className='carousel-item'>
+              <img className='carousel-item__img' src={pcgamer} alt='pcgamer'/>
+            </div>
+            <div className='carousel-item'>
+              <img className='carousel-item__img' src={accesorios} alt='accesorios' />
+            </div>
+          </div >
+            </section >
+        </div>
     )
 }
