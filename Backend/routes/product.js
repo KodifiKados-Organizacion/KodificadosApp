d242e59c457de4988e62bec4b1ea05aef1e096df
unknown <Administrador@QTLOPEZMT.mintrabajo.loc>
2022-11-11 16:41:27 -0500

    instalacion de express-validator

--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -1,40 +1,27 @@
 import express from "express";
-import { ProductModel } from "../models/products.js"
-import { getProducts, createProduct, updateProduct } from "../db.js"
+import { ProductModel } from "../models/products.js";
+import { body, validationResult } from "express-validator";
+import { getProducts, createProduct, updateProduct } from "../db.js";
 
 let Routerproducts = express.Router();
 
-// Routerproducts.get("/",async(req,res)=>{
-
-//   const data= await getProducts()
-//   res.json(data)
-
-// })
-
-// Routerproducts.post("/",async(req,res)=>{
-
-//     createProduct(req.body)
-
-// })
-
-// Routerproducts.put("/:id",async(req,res)=>{
-//     const data=await updateProduct(req.params.id,req.body)
-//     res.json(data)
-// })
-Routerproducts.get("/", async (req, res) => {
-
-    const data = await getProducts()
-    res.json(data)
-
-}).post("/", async (req, res) => {
-
-    createProduct(req.body)
-
-}).put("/:id", async (req, res) => {
-
-    const data = await updateProduct(req.params.id, req.body)
-    res.json(data)
-    
+Routerproducts
+.get("/", async (req, res) => {
+  const data = await getProducts();
+  res.json(data);
 })
+.post("/", body("Nombre").notEmpty(), async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log("JSON.stringify(errors)");
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-export { Routerproducts }
+    createProduct(req.body);
+  })
+  .put("/:id", async (req, res) => {
+    const data = await updateProduct(req.params.id, req.body);
+    res.json(data);
+  });
+
+export { Routerproducts };
